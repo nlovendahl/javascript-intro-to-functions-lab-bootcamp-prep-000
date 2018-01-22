4ca54211b2b92f0a80dbc615c8137c2fd0a7ddd1
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 17:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (`string` === string.toLowerCase()) {
+    console.log(`I can't hear you!`);
     return `I can't hear you!`;
   } else if (`string` === string.toUpperCase()) {
     return `YES INDEED!`;
